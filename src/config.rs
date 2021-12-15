1d48dd742472259e9619c6f9464568c0de8eed98
Åke Forslund <ake.forslund@gmail.com>
2021-12-15 17:01:00 +0100

    Add concept of ConfigStack
    The config stack is a stack of loaded configs. The configs are created
    from a slice with paths (as str). The stack offers a get() method to get
    a value from the first config in the stack containing the key sequence
    requested.

--- src/config.rs
+++ src/config.rs
@@ -20,8 +20,60 @@ fn read_config(path: &Path) -> io::Result<String> {
 
 
 /// Load mycroft configuration file from provided path
+#[allow(dead_code)]
 pub fn load(path: &Path) -> Result<Value> {
     let read_data = read_config(&path).unwrap();
     let config: Value = serde_json::from_str(read_data.as_str())?;
     Ok(config)
+}
+
+
+pub struct ConfigStack {
+    #[allow(dead_code)]
+    files: Vec<String>,
+    #[allow(dead_code)]
+    configs: Vec<Value>
+}
+
+impl ConfigStack {
+    #[allow(dead_code)]
+    /// Create config stack from slice of paths
+    pub fn from_slice(config_paths: &[&str]) -> Result<ConfigStack> {
+        let mut path_vec = Vec::<String>::new();
+        let mut config_vec = Vec::<Value>::new();
+        for config in config_paths {
+            path_vec.push(String::from(*config));
+            config_vec.push(load(Path::new(*config)).unwrap());
+        }
+        Ok(ConfigStack {files: path_vec, configs: config_vec})
+    }
+
+    #[allow(dead_code)]
+    /// Get a value from the first config that has a matching value
+    pub fn get(&self, key_sequence: &[&str])
+            -> std::result::Result<Value, &str> {
+        let mut field;
+        let mut found = false;
+        let mut wrong_type = false;
+        for conf in self.configs.iter() {
+            field = conf;
+            for key in key_sequence.iter() {
+                found = true;
+                match field.get(key) {
+                    Some(res) => field = &res,
+                    _ => found = false
+                }
+
+                let is_last_item = key_sequence.last().unwrap() == key;
+                wrong_type = found && (!is_last_item && !field.is_object());
+                if !found || wrong_type {
+                    break;
+                }
+            }
+            if found && !wrong_type {
+                return Ok((*field).clone())
+            }
+        }
+        Err("Key not found")
+    }
 }
