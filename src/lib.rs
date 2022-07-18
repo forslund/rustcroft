e99d70c9dce2c3deae077475606f0e0c05413a73
Åke Forslund <ake.forslund@gmail.com>
2022-07-18 13:22:06 +0200

    Import the tests to run them

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,9 @@ pub mod skill;
 pub mod config;
 pub mod dialog;
 
+#[cfg(test)]
+mod test;
+
 pub use mycroft_message::MycroftMessage;
 pub use adapt_interface::{AdaptKeyword, AdaptIntent};
 pub use skill::{MsgHandler, EventHandler, start_skill};
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
-
-
